76b41b2790364806a0225777a7b28f1037cb6d31
MManai93 <m.manai@dreamcentury.com>
2015-10-16 19:04:39 +0200

    correction js2

--- Web/loadNewComments.js
+++ Web/loadNewComments.js
@@ -5,51 +5,43 @@ function loadNewComments()
     var author;
     var email;
     var hreference;
+    var affichagedate;
     console.log(news);
     console.log(comment);
     jQuery.post("getNewComments.html", {news_id: news, comment_id_last: comment},function(data){
         console.log(data);
-
-
         $.each(data,function(key,value) {
             console.log(value);
-            if (value.ghost_author != null) {
+            if (value.ghost_author != null)
+            {
                 email = value.ghost_email;
                 author = value.ghost_author;
                 hreference = '/profil-ghost-' + value.ghost_author + '.html';
             }
-            else {
+            else
+            {
                 email = value.member_email;
                 author = value.member_login;
                 hreference = '/admin/profil-' + value.member_id + '.html';
+            }
+            if(value.dateAdd.date!=value.dateModif.date)
+            {
+                affichagedate='le'+value.dateAdd.date+'<em> Modifiée le '+value.dateModif.date+'</em>' ;
+            }
+            else
+            {
+                affichagedate='le '+value.dateAdd.date;
             }
             /*passer les attributs de href et ghost (true ou false) dans les attributs de la balise fieldset*/
             $('<fieldset></fieldset>').insertAfter($('.comment:last'))
                 .addClass("comment")
                 .attr('comment-id', value.comment_id)
                 .attr('news-id', value.news_id)
-                .append('<legend>Poste par <strong><a href="'+hreference+'">'+author+'</a></strong> le '+value.dateAdd.date+'<em>'+email+'</em></legend>')
+                .append('<legend>Poste par <strong><a href="'+hreference+'"> '+author+'</a></strong>'+affichagedate+'<em> '+email+'</em></legend>')/*format date*/
                 .append('<p>'+value.content+'</p>')
-
-            /*$('<fieldset></fieldset>')
-             .addClass('comment')
-             .attr('data-id',comment.id)
-             .append(
-             $('<legend></legend>')
-             .append(
-             'Poste par ',
-             $('<a></a>')
-             .attr('href','/member-' + comment.auteur + '.html')
-             .html( comment.auteur)
-             .css('font-weight','bold'),
-             ' le ' + comment.date
-             ),
-             $('<p></p>')
-             .html(comment.contenu)*/
         })
 
     },"json");
 
     //setTimeout(loadNewComments,5000);
-
 }
